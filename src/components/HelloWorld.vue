4b750cfd8338dc73339b83a0aaba87b0ae02a2e3
henribru <6639509+henribru@users.noreply.github.com>
2022-06-03 07:47:20 +0200

    Add aria-label

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button @click="count++">count is: {{ count }}</button>
+  <button @click="count++" aria-label="foo">count is: {{ count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
